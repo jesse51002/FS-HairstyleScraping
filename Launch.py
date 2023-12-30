744f2af2c51d3cea1bfc3d3a895d5712e7e50541
Jesse Musa <jessemusa2@gmail.com>
2023-12-30 02:14:48 -0600

    Finished launchers

--- Launch.py
+++ Launch.py
@@ -40,75 +40,93 @@ with open(STYLES_FILES,'r') as file:
    styles = [x.strip() for x in file.readlines()]
 
 # Scaper
-def ScrapeAndClean(raw_dir, output_dir, dir, callback):
-    # Scapes and store into the folder
-    scrape(query=f"{style} hairstyle{directions[dir]}", folder=raw_dir)
-    print("FINISHED SCAPING")
+def ScrapeAndClean(style, raw_style_dir, clean_style_dir, callback):
+    for dir in directions.keys():
+        # Scapes and store into the folder
+        raw_dir = os.path.join(raw_style_dir, dir)
+        if not os.path.isdir(raw_dir):
+            os.makedirs(raw_dir)
         
-    print("Scaped Images:", os.listdir(raw_dir))
+        clean_dir = os.path.join(clean_style_dir, dir)
+        if not os.path.isdir(clean_dir):
+            os.makedirs(clean_dir)
         
-    for img in os.listdir(raw_dir):
-        if callback["Exit"]:
-            callback["Finished"] = True
-            return
+        scrape(query=f"{style} hairstyle{directions[dir]}", folder=raw_dir)
+        print("FINISHED SCAPING")
             
-        raw_img = os.path.join(raw_dir, img)
-        cleaned = clean_img(raw_img)
+        print("Scaped Images:", os.listdir(raw_dir))
             
-        # os.remove(raw_img)
-        
-        # If image was not accepted continue
-        if cleaned is None:
-            continue
+        for img in os.listdir(raw_dir):
+            if callback["Exit"]:
+                return
+                
+            raw_img = os.path.join(raw_dir, img)
+            cleaned = clean_img(raw_img)
+                
+            # os.remove(raw_img)
+            
+            # If image was not accepted continue
+            if cleaned is None:
+                continue
 
-        cv2.imwrite(os.path.join(output_dir, img), cleaned)
-        
-        time.sleep(0.1)
+            cv2.imwrite(os.path.join(clean_dir, img), cleaned)
             
-    callback["Finished"] = True
+            time.sleep(0.1)
+                
+        callback["Finished"][dir] = True
 
          
 # Style parser
 # Scapes, cleans and shows cleaned image
 def parse_style(style):
-    for dir in directions.keys():  
-        callback = {
-            "Finished": False,
+    end_early = False
+    
+    thread = None
+    callback = {
+            "Finished": {},
             "Exit": False,
         }
+    
+    raw_style_dir = os.path.join(RAW_IMAGES_DIR, style)
+    if not os.path.isdir(raw_style_dir):
+        os.makedirs(raw_style_dir)
+        
+    clean_style_dir = os.path.join(CLEAN_IMAGES_DIR, style)
+    if not os.path.isdir(clean_style_dir):
+        os.makedirs(clean_style_dir)
+        
+    accepted_style_dir = os.path.join(ACCEPTED_IMAGES_DIR, style)
+    if not os.path.isdir(accepted_style_dir):
+        os.makedirs(accepted_style_dir)
+    
+    for dir in directions.keys():
+        callback["Finished"][dir] = False
+        
+    thread = threading.Thread(target=ScrapeAndClean, args=(style, raw_style_dir, clean_style_dir, callback,))
+    thread.start()
+    
+    for dir in directions.keys(): 
+        
+        clean_dir = os.path.join(clean_style_dir, dir)
+        if not os.path.isdir(clean_dir):
+            os.makedirs(clean_dir)
+        
+        accepted_dir = os.path.join(accepted_style_dir, dir)
+        if not os.path.isdir(accepted_dir):
+            os.makedirs(accepted_dir)
         
         print(f"STARTING {style} in the {dir} direction")
         
         print("Creating Directories...")
-        
-        raw_dir = os.path.join(RAW_IMAGES_DIR, style, dir)
-        if not os.path.isdir(raw_dir):
-            os.makedirs(raw_dir)
-        
-        clean_dir = os.path.join(CLEAN_IMAGES_DIR, style, dir)
-        if not os.path.isdir(clean_dir):
-            os.makedirs(clean_dir)
-        
-        accepted_dir = os.path.join(ACCEPTED_IMAGES_DIR, style, dir)
-        if not os.path.isdir(accepted_dir):
-            os.makedirs(accepted_dir)
-        
+
         print("Scraping...")
         
-        cur_thread = threading.Thread(target=ScrapeAndClean, args=(raw_dir, clean_dir, dir, callback,))
-        cur_thread.start()
-        
-        end_early = False
-        
-        while not callback["Finished"] or len(os.listdir(clean_dir)) > 0:
-            if end_early:
-                break   
-            
+        while not callback["Finished"][dir] or len(os.listdir(clean_dir)) > 0:
             if len(os.listdir(clean_dir)) == 0:
-                plt.imshow(np.random.rand(500,500))
+                plt.imshow(np.zeros((500,500)))
                 plt.xlabel(f"Waiting... 'H' to end ::: Any other key to continue")
-                plt.ion()
                 plt.show()
+                plt.pause(0.5)
                 while msvcrt.kbhit():
                     msvcrt.getch()
                 key = msvcrt.getch().decode("utf-8")
@@ -117,7 +135,6 @@ def parse_style(style):
                     end_early = True
                     callback["Exit"] = True
                     break
-                plt.close()
                 continue
             
             img_name = os.listdir(clean_dir)[0]
@@ -130,7 +147,6 @@ def parse_style(style):
             
             plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
             plt.xlabel(f"{style} hairstyle in {dir} ||| 'A' to Accept ::: 'R' to reject ::: 'H' to end")
-            plt.ion()
             plt.show()
             
             while True:
@@ -155,29 +171,42 @@ def parse_style(style):
                     break
                 
                 print("Invalid Key, Input valid key")
-                            
-            plt.close()     
+                                        
+            if end_early:
+                break      
+            
+        if end_early:
+            break
+    
+    if thread.is_alive():
+        thread.join()
         
-        print("here")
-        if cur_thread.is_alive():
-            cur_thread.join()
+def launch():
+    plt.imshow(np.zeros((500,500)))
+    plt.xlabel(f"Waiting... 'H' to end ::: Any other key to continue")
+    plt.ion()
+    plt.show()
+    plt.pause(0.5)
+
+    for style in styles:
+        if styles in progress["scraped_styles"]:
+            print(f"Already completed {style}. Skipping...")
+            continue
         
-       
-for style in styles:
-    if styles in progress["scraped_styles"]:
-        print(f"Already completed {style}. Skipping...")
-        continue
+        print(f"Starting {style}. \n Any key to continue or 'H' to stop...")
+        while msvcrt.kbhit():
+            msvcrt.getch()
+        key = msvcrt.getch().decode("utf-8")
+        if key == "h" or key == "H":
+            break
+        
+        parse_style(style)
+        progress["scraped_styles"].append(style)
+        
+    plt.close()
     
-    print(f"Starting {style}. \n Any key to continue or 'H' to stop...")
-    while msvcrt.kbhit():
-        msvcrt.getch()
-    key = msvcrt.getch().decode("utf-8")
-    if key == "h" or key == "H":
-        break
-    
-    parse_style(style)
-    progress["scraped_styles"].append(style)
-    
+if __name__ == "__main__":
+    launch()
     
 # Write save file
 with open(JSON_SAVE_FILE,'w') as json_file:
