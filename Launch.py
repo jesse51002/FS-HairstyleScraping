8276ef493f9b60c528abde15fded279ac8b92eae
Jesse Musa <jessemusa2@gmail.com>
2023-12-31 03:16:24 -0600

    Base app done

--- Launch.py
+++ Launch.py
@@ -24,7 +24,7 @@ directions = {
 }
 
 progress = {
-    "scraped_styles": []
+    "scraped_styles": [],
     }
 
 # Load progress
@@ -40,175 +40,243 @@ with open(STYLES_FILES,'r') as file:
    styles = [x.strip() for x in file.readlines()]
 
 # Scaper
-def ScrapeAndClean(style, raw_style_dir, clean_style_dir, callback):
-    for dir in directions.keys():
-        # Scapes and store into the folder
-        raw_dir = os.path.join(raw_style_dir, dir)
-        if not os.path.isdir(raw_dir):
-            os.makedirs(raw_dir)
+def ScrapeAndClean(global_data):
+    for style in styles:
+        # End early if wants to exit
+        if global_data["Exit"]:
+            return
         
-        clean_dir = os.path.join(clean_style_dir, dir)
-        if not os.path.isdir(clean_dir):
-            os.makedirs(clean_dir)
+        global_data["Finished"][style] = {"Completed": False}
         
-        scrape(query=f"{style} hairstyle{directions[dir]}", folder=raw_dir)
-        print("FINISHED SCAPING")
-            
-        print("Scaped Images:", os.listdir(raw_dir))
-            
-        for img in os.listdir(raw_dir):
-            if callback["Exit"]:
+        for direction in directions.keys():
+            # End early if wants to exit
+            if global_data["Exit"]:
                 return
-                
-            raw_img = os.path.join(raw_dir, img)
-            cleaned = clean_img(raw_img)
-                
-            # os.remove(raw_img)
             
-            # If image was not accepted continue
-            if cleaned is None:
-                continue
-
-            cv2.imwrite(os.path.join(clean_dir, img), cleaned)
+            global_data["Finished"][style][direction] = False
             
+            print(f"Starting Scape and Clean for {style} in {direction}")
+            
+            # Creatses directories
+            raw_dir = os.path.join(RAW_IMAGES_DIR, style, direction)
+            if not os.path.isdir(raw_dir):
+                os.makedirs(raw_dir)
+                
+            clean_dir = os.path.join(CLEAN_IMAGES_DIR, style, direction)
+            if not os.path.isdir(clean_dir):
+                os.makedirs(clean_dir)
+            
+            # Scapes the stlye and direction
+            SCStyDir(style, direction, raw_dir, clean_dir,global_data)
+            
+            global_data["Finished"][style][direction] = True
+            
+        global_data["Finished"][style]["Completed"] = True
+    
+    global_data["CleanFinished"] = True
+            
+    
+    
+            
+# Scrapes a specific 
+def SCStyDir(style, direction, raw_dir, clean_dir, global_data):
+    
+    
+    # Scapes and store into the folder
+    
+    # Thread Lock for downloaded_paths
+    lock = threading.Lock()
+    downloaded_paths = []
+    finished = {"Finished":False}
+    
+    # For Scape to tell when scraping is finsihed
+    def finished_callback():
+        finished["Finished"] = True
+        
+    # For scrape to tell when an image was downloaded
+    def img_download_callback(path):
+        lock.acquire()
+        downloaded_paths.append(path)
+        lock.release()
+        
+    
+    # Scapes and downloads
+    hide_browser = False  
+    scrape_thread = threading.Thread(target=scrape, args=(global_data, f"{style}{directions[direction]}", raw_dir, img_download_callback, finished_callback, hide_browser))
+    scrape_thread.start()
+    
+    # Loop until scraping is finished and all cleaned
+    while not finished["Finished"] or len(downloaded_paths) > 0 :
+        # End early if wants to exit
+        if global_data["Exit"]:
+            return
+        
+        # if nothing has been downloaded yet then wait
+        if len(downloaded_paths) == 0:
             time.sleep(0.1)
-                
-        callback["Finished"][dir] = True
+            continue
+        
+        # Gets image and cleans it
+        raw_img = downloaded_paths[0]
+        cleaned = clean_img(raw_img)
+        
+        # Removes the raw img after it is cleaned       
+        # os.remove(raw_img)
+                    
+        # If image was not accepted continue
+        if cleaned is not None:
+           # Save the cleaned image
+            cv2.imwrite(os.path.join(clean_dir, os.path.basename(raw_img)), cleaned)
+        
+        
+        # Remove the path just claned
+        lock.acquire()
+        downloaded_paths.pop(0)
+        lock.release()
+        time.sleep(0.1)
+                        
+    
+    scrape_thread.join()
+    
+# Finds valid cleaned image to show to user
+def find_image(global_data):
+    # Loops through all the styles folder
+    for style in os.listdir(CLEAN_IMAGES_DIR):
+        style_dir = os.path.join(CLEAN_IMAGES_DIR, style)
+        
+        # Empty style directory handling 
+        if len(os.listdir(style_dir)) == 0:
+            # If its finsihed and its empty then delete the folder
+            if style not in global_data["Finished"] or global_data["Finished"][style]["Completed"]:
+                os.rmdir(style_dir)
+            continue
+        
+        # Loops throught all the styles directions
+        for direction in os.listdir(style_dir):
+            direction_dir = os.path.join(style_dir, direction)
+            
+            # Empty style direction directory handling 
+            if len(os.listdir(direction_dir)) == 0:
+                # If its finsihed and its empty then delete the folder
+                if direction not in global_data["Finished"][style] or  global_data["Finished"][style][direction]:
+                    os.rmdir(direction_dir)
+                continue
+            
+            # Uses the first found image
+            return os.path.join(direction_dir, os.listdir(direction_dir)[0]), style, direction
+        
+    return None, None, None
 
-         
+    
+
 # Style parser
 # Scapes, cleans and shows cleaned image
-def parse_style(style):
-    end_early = False
+def parse_style(global_data): 
     
-    thread = None
-    callback = {
-            "Finished": {},
-            "Exit": False,
-        }
+    plt.imshow(np.zeros((500,500)))
+    plt.xlabel(f"Starting...")
+    plt.ion()
+    plt.show()
+    plt.pause(0.2)
     
-    raw_style_dir = os.path.join(RAW_IMAGES_DIR, style)
-    if not os.path.isdir(raw_style_dir):
-        os.makedirs(raw_style_dir)
+    while not global_data["Exit"]:
+        img_pth, style, direction = find_image(global_data)
         
-    clean_style_dir = os.path.join(CLEAN_IMAGES_DIR, style)
-    if not os.path.isdir(clean_style_dir):
-        os.makedirs(clean_style_dir)
+        if img_pth is None:
+            if global_data["CleanFinished"]:
+                break
+            
+            plt.imshow(np.zeros((500,500)))
+            plt.xlabel(f"Waiting for cleaned image... H to stop")
+            
+            
+            if msvcrt.kbhit():
+                key = msvcrt.getch().decode("utf-8")
+                if key == "H" or key == "h":
+                    global_data["Exit"] = True
+                    break
+            
+            plt.pause(0.2)
+            
+            continue
         
-    accepted_style_dir = os.path.join(ACCEPTED_IMAGES_DIR, style)
-    if not os.path.isdir(accepted_style_dir):
-        os.makedirs(accepted_style_dir)
-    
-    for dir in directions.keys():
-        callback["Finished"][dir] = False
-        
-    thread = threading.Thread(target=ScrapeAndClean, args=(style, raw_style_dir, clean_style_dir, callback,))
-    thread.start()
-    
-    for dir in directions.keys(): 
-        
-        clean_dir = os.path.join(clean_style_dir, dir)
-        if not os.path.isdir(clean_dir):
-            os.makedirs(clean_dir)
-        
-        accepted_dir = os.path.join(accepted_style_dir, dir)
+        accepted_dir = os.path.join(ACCEPTED_IMAGES_DIR, style, direction)
         if not os.path.isdir(accepted_dir):
             os.makedirs(accepted_dir)
         
-        print(f"STARTING {style} in the {dir} direction")
-        
-        print("Creating Directories...")
-
-        print("Scraping...")
-        
-        while not callback["Finished"][dir] or len(os.listdir(clean_dir)) > 0:
-            if len(os.listdir(clean_dir)) == 0:
-                plt.imshow(np.zeros((500,500)))
-                plt.xlabel(f"Waiting... 'H' to end ::: Any other key to continue")
-                plt.show()
-                plt.pause(0.5)
-                while msvcrt.kbhit():
-                    msvcrt.getch()
-                key = msvcrt.getch().decode("utf-8")
+        img = cv2.imread(img_pth)
                 
-                if key == "H" or key == "h":
-                    end_early = True
-                    callback["Exit"] = True
-                    break
+        accepted_pth = os.path.join(accepted_dir, os.path.basename(img_pth))
+                
+        print("Displaying the image")
+                
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.xlabel(f"{style} hairstyle in {direction} ||| 'A' to Accept ::: 'R' to reject ::: 'H' to end")
+        
+        while True:
+            plt.pause(0.2)
+            
+            key = None
+            if msvcrt.kbhit():
+                key = msvcrt.getch().decode("utf-8")
+            else:
                 continue
             
-            img_name = os.listdir(clean_dir)[0]
-            img_pth = os.path.join(clean_dir, img_name)
-            img = cv2.imread(img_pth)
-            
-            accepted_pth = os.path.join(accepted_dir, img_name)
-            
-            print("Displaying the image")
-            
-            plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-            plt.xlabel(f"{style} hairstyle in {dir} ||| 'A' to Accept ::: 'R' to reject ::: 'H' to end")
-            plt.show()
-            
-            while True:
-                plt.pause(0.5)
-                while msvcrt.kbhit():
-                    msvcrt.getch()
-                key = msvcrt.getch().decode("utf-8")
-                # Moves file to accepted
-                if key == "A" or key == "a":
-                    if os.path.isfile(accepted_pth):
-                        os.remove(accepted_pth)
-                    os.rename(img_pth, accepted_pth)
-                    break             
-                # If rejected delete the image
-                elif key == "R" or key == "r":
-                    os.remove(img_pth)
-                    break
-                # Forcefully ends the application
-                elif key == "H" or key == "h":
-                    end_early = True
-                    callback["Exit"] = True
-                    break
-                
+            # Moves file to accepted
+            if key == "A" or key == "a":
+                # Write/ Overwrite accepted image
+                if os.path.isfile(accepted_pth):
+                    os.remove(accepted_pth)
+                os.rename(img_pth, accepted_pth)
+                break             
+            # If rejected delete the image
+            elif key == "R" or key == "r":
+                os.remove(img_pth)
+                break
+            # Forcefully ends the application
+            elif key == "H" or key == "h":
+                global_data["Exit"] = True
+                break
+            else:     
                 print("Invalid Key, Input valid key")
-                                        
-            if end_early:
-                break      
             
-        if end_early:
-            break
-    
-    if thread.is_alive():
-        thread.join()
-        
-def launch():
-    plt.imshow(np.zeros((500,500)))
-    plt.xlabel(f"Waiting... 'H' to end ::: Any other key to continue")
-    plt.ion()
-    plt.show()
-    plt.pause(0.5)
-
-    for style in styles:
-        if styles in progress["scraped_styles"]:
-            print(f"Already completed {style}. Skipping...")
-            continue
-        
-        print(f"Starting {style}. \n Any key to continue or 'H' to stop...")
-        while msvcrt.kbhit():
-            msvcrt.getch()
-        key = msvcrt.getch().decode("utf-8")
-        if key == "h" or key == "H":
-            break
-        
-        parse_style(style)
-        progress["scraped_styles"].append(style)
+            
         
     plt.close()
+    plt.ioff()
+
+# Starts the application
+def launch():
+    global_data = {
+        "Finished": {},
+        "Exit": False,
+        "CleanFinished": False
+    }
+    
+    # Makes importart paths
+    if not os.path.exists(RAW_IMAGES_DIR):
+        os.makedirs(RAW_IMAGES_DIR)
+    if not os.path.exists(CLEAN_IMAGES_DIR):
+        os.makedirs(CLEAN_IMAGES_DIR)
+    if not os.path.exists(ACCEPTED_IMAGES_DIR):
+        os.makedirs(ACCEPTED_IMAGES_DIR)
+    
+    # Scapes and downloads
+    scrape_clean_thread = threading.Thread(target=ScrapeAndClean, args=(global_data,))
+    scrape_clean_thread.start()
+
+    parse_style(global_data)
+    
+    
+    print("Waiting for Clean and Scrape thread to end...")
+    scrape_clean_thread.join()
+        
+    plt.close()  
     
 if __name__ == "__main__":
     launch()
     
-# Write save file
-with open(JSON_SAVE_FILE,'w') as json_file:
-    progress = json.dump(progress, json_file)
-   
+    # Write save file
+    with open(JSON_SAVE_FILE,'w') as json_file:
+        progress = json.dump(progress, json_file)
+    
--- Scrapper.py
+++ Scrapper.py
@@ -1,20 +1,33 @@
-from pinscrape import pinscrape
 import os
+from GoogleImageScraper.GoogleImageScraper import GoogleImageScraper
+from GoogleImageScraper.patch import webdriver_executable
+
+
 
 RAW_IMAGES_DIR = "./images/raw_images"
 
-def scrape(query="wavy hair side profile", folder=RAW_IMAGES_DIR ,  img_count=20):
+WEBDRIVER_PATH = os.path.normpath(os.path.join(os.getcwd(), "GoogleImageScraper", 'webdriver', webdriver_executable()))
 
-    images_dir = os.path.join(folder)
+SCRAPE_COUNT = 10
+HIDE_BROWSER = True
+MIN_RESOLUTION = (500,700)
+MAX_RESOLUTION = (2000,2000)
+
+
+def scrape (global_data=None,query="long bob hairstyle", folder=RAW_IMAGES_DIR , download_callback=None, finished_callback=None, hide_browser=HIDE_BROWSER):
+    image_scraper = GoogleImageScraper(WEBDRIVER_PATH,"","",SCRAPE_COUNT,HIDE_BROWSER,MIN_RESOLUTION,MAX_RESOLUTION)
     
-    details = pinscrape.scraper.scrape(query, images_dir, max_images=img_count)
-
-    if details["isDownloaded"]:
-        print(f"\nDownloading completed for {query} !!")
-        print(f"\nTotal urls found: {len(details['extracted_urls'])}")
-        print(f"\nTotal images downloaded (including duplicate images): {len(details['url_list'])}")
-    else:
-        print("\nNothing to download !!")
-        
+    image_scraper.headless = hide_browser
+    image_scraper.search_key = query
+    image_scraper.image_path = folder
+    image_scraper.img_download_callback = download_callback
+    image_scraper.finish_scrape_callback = finished_callback
+    
+    image_scraper.find_image_urls(global_data)
+    
+            
 if __name__ == "__main__":
-    scrape()
+    output = os.path.join(RAW_IMAGES_DIR, "test")
+    if not os.path.isdir(output):
+        os.makedirs(output)
+    scrape(folder=output, img_count=5)
