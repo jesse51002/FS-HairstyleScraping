90179d5e6a8769a395bfffc12cc74bfa8d7779ca
Jesse Musa <jessemusa2@gmail.com>
2024-03-24 21:24:40 -0500

    finished pose model

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-#FROM nvidia/cuda:12.1.0-runtime-ubuntu22.04 as deps
-
-
 ARG PYTORCH="2.2.1"
 ARG CUDA="12.1"
 ARG CUDNN="8"
@@ -10,17 +7,17 @@ FROM pytorch/pytorch:${PYTORCH}-cuda${CUDA}-cudnn${CUDNN}-devel as deps
 SHELL ["/bin/bash", "--login", "-c"]
 
 # Step 1. Set up Ubuntu
-RUN apt update && apt install --yes wget ssh git git-lfs vim build-essential gcc-12 ninja-build
+RUN apt update && apt install --yes wget ssh git vim build-essential gcc-12 ninja-build libgl1 libgtk2.0-dev pkg-config
 
 FROM deps as compiler
 
 WORKDIR /root
 
 ENV TORCH_CUDA_ARCH_LIST="7.5;8.0;8.6+PTX;8.9;9.0"
+# ENV FORCE_CUDA="1"
 
-RUN pip install cmake boto3 ftfy gdown imageio-ffmpeg matplotlib opencv-python-headless pandas scipy scikit-learn scikit-image sixdrepnet supervision timm tk ultralytics typing_extensions ipython
+RUN pip install cmake boto3 ftfy gdown imageio-ffmpeg matplotlib pandas scipy scikit-learn scikit-image sixdrepnet supervision timm tk ultralytics typing_extensions ipython
 RUN pip install git+https://github.com/openai/CLIP.git
-
 
 RUN git clone https://github.com/open-mmlab/mmcv.git
 WORKDIR /root/mmcv
@@ -35,9 +32,12 @@ RUN mkdir -p /mmpose/data
 RUN pip install --no-cache-dir -e .
 WORKDIR /root
 
-WORKDIR /root/FS-HairstyleScraping
 
-RUN apt install --yes libgl1 
+RUN pip uninstall -y opencv-python opencv-python-headless
+RUN pip install opencv-python-headless
+
+
+WORKDIR /root/FS-HairstyleScraping
 
 ADD . .
 
--- src/ViTPose/pose_extract.py
+++ src/ViTPose/pose_extract.py
@@ -16,43 +16,129 @@ import cv2
 import time
 import mmcv
 import torch
-from mmcv import Config, DictAction
-from mmcv.cnn import fuse_conv_bn
-from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
-from mmcv.runner import get_dist_info, init_dist, load_checkpoint
+import torch.nn as nn
+import numpy as np
+from huggingface_hub import hf_hub_download
 
-from mmpose.models import build_posenet
-
-try:
-    from mmcv.runner import wrap_fp16_model
-except ImportError:
-    warnings.warn('auto_fp16 from mmpose will be deprecated from v0.15.0'
-                  'Please install mmcv>=1.1.4')
-    from mmpose.core import wrap_fp16_model
-    
+# from mmdet.apis import inference_detector, init_detector
+from mmpose.apis import (inference_top_down_pose_model, init_pose_model,
+                         process_mmdet_results, vis_pose_result)
 
 import Constants
 
-MODEL_PATH = './src/ViTPose/vitpose+_large.pth'
-CONFIG_PATH = './src/ViTPose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/vitPose+_large_coco+aic+mpii+ap10k+apt36k+wholebody_256x192_udp.py'
-
 class pose_model(Model):
-    
+    MODEL_DICT = {
+        'ViTPose-B (single-task train)': {
+            'config':
+            'src/ViTPose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_base_coco_256x192.py',
+            'model': 'models/vitpose-b.pth',
+        },
+        'ViTPose-L (single-task train)': {
+            'config':
+            'src/ViTPose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_large_coco_256x192.py',
+            'model': 'models/vitpose-l.pth',
+        },
+        'ViTPose-B (multi-task train, COCO)': {
+            'config':
+            'src/ViTPose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_base_coco_256x192.py',
+            'model': 'models/vitpose-b-multi-coco.pth',
+        },
+        'ViTPose-L (multi-task train, COCO)': {
+            'config':
+            'src/ViTPose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_large_coco_256x192.py',
+            'model': 'models/vitpose-l-multi-coco.pth',
+        },
+    }
+
     def __init__(self):
-        cfg = Config.fromfile(CONFIG_PATH)
-        self.model = build_posenet(cfg.model)
-        fp16_cfg = cfg.get('fp16', None)
-        if fp16_cfg is not None:
-            wrap_fp16_model(self.model)
-        load_checkpoint(self.model, MODEL_PATH, map_location='cpu')
-        self.model = fuse_conv_bn(self.model)
-        self.model = MMDataParallel(self.model, device_ids=[0])
+        self.device = torch.device(
+            'cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model_name = 'ViTPose-B (multi-task train, COCO)'
+        self.model = self._load_model(self.model_name)
         
-    @torch.no_grad()
-    def inference(self, img):
-        output = self.model(return_loss=False, **img)
-        print(output)
-        return output
+        # self.det_model = person_det_model()
+
+    def _load_all_models_once(self) -> None:
+        for name in self.MODEL_DICT:
+            self._load_model(name)
+            
+    def _load_model(self, name: str) -> nn.Module:
+        d = self.MODEL_DICT[name]
+        ckpt_path = hf_hub_download('public-data/ViTPose',
+                                                    d['model'])
+        model = init_pose_model(d['config'], ckpt_path, device=self.device)
+        return model
+
+    def predict_pose_and_visualize(
+        self,
+        image: np.ndarray,
+        det_results: list[np.ndarray],
+        box_score_threshold: float,
+        kpt_score_threshold: float,
+        vis_dot_radius: int,
+        vis_line_thickness: int,
+    ) -> tuple[list[dict[str, np.ndarray]], np.ndarray]:
+        out = self.inference(image, det_results, box_score_threshold)
+        vis = self.visualize_pose_results(image, out, kpt_score_threshold,
+                                          vis_dot_radius, vis_line_thickness)
+        return out, vis
+
+    def inference(
+            self,
+            image: np.ndarray,
+            # det_results: list[np.ndarray],
+            box_score_threshold: float = 0.5) -> list[dict[str, np.ndarray]]:
+        # image = image[:, :, ::-1]  # RGB -> BGR
+        
+        det_results = np.array([[[0,0, 1024, 1024, 1.0]]]) # self.person_det_model.inference(image)
+        
+        person_results = process_mmdet_results(det_results, 1)
+        
+        out, _ = inference_top_down_pose_model(self.model,
+                                               image,
+                                               person_results=person_results,
+                                               bbox_thr=box_score_threshold,
+                                               format='xyxy')
+        return out# [0]['keypoints'], 
+    
+    def index_to_dic(self, pose2d):
+        
+        return {
+            "nose": pose2d[0],
+            "left_eye": pose2d[1],
+            "right_eye": pose2d[2],
+            "left_ear": pose2d[3],
+            "right_ear": pose2d[4],
+            "left_shoudler": pose2d[5],
+            "right_shoudler": pose2d[6],
+            "left_elbow": pose2d[7],
+            "right_elbow": pose2d[8],
+            "left_hand": pose2d[9],
+            "right_hand": pose2d[10],
+            "left_waist": pose2d[11],
+            "right_waist": pose2d[12],
+            "left_knee": pose2d[13],
+            "right_knee": pose2d[14],
+            "left_foot": pose2d[15],
+            "right_foot": pose2d[16],
+        }
+        
+
+    def visualize_pose_results(self,
+                               image: np.ndarray,
+                               pose_results: list[np.ndarray],
+                               kpt_score_threshold: float = 0.3,
+                               vis_dot_radius: int = 4,
+                               vis_line_thickness: int = 1) -> np.ndarray:
+        #image = image[:, :, ::-1]  # RGB -> BGR
+        vis = vis_pose_result(self.model,
+                              image,
+                              pose_results,
+                              kpt_score_thr=kpt_score_threshold,
+                              radius=vis_dot_radius,
+                              thickness=vis_line_thickness) 
+        return vis #[:, :, ::-1]  # BGR -> RGB
+         
     
     
 if __name__ == "__main__":
@@ -72,10 +158,15 @@ if __name__ == "__main__":
         print("Img shape:",img.shape)
 
         start = time.time()
-        output = model.inference(img)
+        raw_output = model.inference(img)
+
+        positions = raw_output[0]["keypoints"][:, :2].astype(int)
         # output_img = output_img.detach().cpu().numpy()[0]
         print("Took:", time.time() - start)
-        # print("output:", output.shape)
+        print("output:", positions)
+        
+        vis = model.visualize_pose_results(img, raw_output)
+        cv2.imwrite("pose_example.jpg", vis)
         
         exit()
         # setting values to rows and column variables 
